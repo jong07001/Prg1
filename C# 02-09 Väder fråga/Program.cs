f6b0abc647fadf9d094070923d98cc5c3b613e04
jong07001 <jong07001@utb.vaxjo.se>
2024-09-02 09:57:17 +0200

    Add files via upload

---
+++ C# 02-09 Väder fråga/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    Console.WriteLine("Är det fint väder?");
+    string svar1 = Console.ReadLine();
+    
+    if (svar1=="j"||svar1=="J"){
+        Console.WriteLine("Vi går på picknick!");
+    }
+        else if (svar1=="n"||svar1=="N"){
+            Console.WriteLine("Vi stannar inne och läser en bok");
+        }
+        else{
+            Console.WriteLine("Jag förstår inte");
+        }
+
+
+        
+    }
